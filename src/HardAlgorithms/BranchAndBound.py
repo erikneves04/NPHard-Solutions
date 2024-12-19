1134cb744a5b034e614b459276bc48b41e7b21e6
Erik Neves <erikrrn04@gmail.com>
2024-12-19 18:21:25 -0300

    create hard algorithms module

---
+++ src/HardAlgorithms/BranchAndBound.py
@@ -0,0 +1,12 @@
+from ProblemManager.ProblemManager import ProblemManager
+
+class BranchAndBound:
+    def __init__(self):
+        self.problemManager = ProblemManager()
+
+    def solve(self, problem_path):
+        problem = self.problemManager.ReadProblem(problem_path)
+
+        # TODO: Implementar esse algoritmo
+
+        return None
---
+++ src/HardAlgorithms/TwiceAroundTheTree.py
@@ -0,0 +1,12 @@
+from ProblemManager.ProblemManager import ProblemManager
+
+class TwiceAroundTheTree:
+    def __init__(self):
+        self.problemManager = ProblemManager()
+
+    def solve(self, problem_path):
+        problem = self.problemManager.ReadProblem(problem_path)
+
+        # TODO: Implementar esse algoritmo
+
+        return None
