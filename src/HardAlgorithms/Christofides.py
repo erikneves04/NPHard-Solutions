fe4b0f4c0f06454b14ccd8a9e26ac2a7dd82379d
Erik Neves <erikrrn04@gmail.com>
2025-01-02 12:23:32 -0300

    add christofides algorithm

--- src/HardAlgorithms/Christofides.py
+++ src/HardAlgorithms/Christofides.py
@@ -1,12 +1,41 @@
+import networkx as nx
+
+from Utils.PrimAlgorithm import PrimAlgorithm
+from Utils.PerfectMatching import PerfectMatching
+from Utils.HamiltonianPriceCalculator import HamiltonianPriceCalculator
 from ProblemManager.ProblemManager import ProblemManager
 
 class Christofides:
     def __init__(self):
         self.problemManager = ProblemManager()
+        self.primAlgorithm = PrimAlgorithm()
+        self.matchAlgorithm = PerfectMatching()
 
-    def solve(self, problem_path):
-        problem = self.problemManager.ReadProblem(problem_path)
+    def __find_eulerian_circuit(self, graph):
+        return list(nx.eulerian_circuit(graph))
 
-        # TODO: Implementar esse algoritmo
+    def __convert_to_hamiltonian_circuit(self, eulerian):
+        visited = set()
+        hamiltonian_circuit = []
 
-        return None
+        for u, v in eulerian:
+            if u not in visited:
+                hamiltonian_circuit.append(u)
+                visited.add(u)
+        hamiltonian_circuit.append(hamiltonian_circuit[0])  
+        
+        return hamiltonian_circuit
+
+    def solve(self, problem):
+        graph = self.problemManager.ReadProblem(problem)
+        
+        mst, total_weight = self.primAlgorithm.BuildMST(graph)
+        matching = self.matchAlgorithm.BuildPerfectMathing(graph, mst)
+
+        eulerian_graph = nx.MultiGraph()
+        eulerian_graph.add_edges_from([(u, v, {'weight': w}) for u, v, w in mst + matching])
+        
+        eulerian = self.__find_eulerian_circuit(eulerian_graph)
+        hamiltonian = self.__convert_to_hamiltonian_circuit(eulerian)
+
+        return hamiltonian
